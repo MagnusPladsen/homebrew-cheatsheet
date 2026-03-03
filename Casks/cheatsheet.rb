650385ac9acff8b695db3437bed0f00f0374fe45
Magnus Pladsen <93226629+MagnusPladsen@users.noreply.github.com>
2026-03-03 15:11:12 +0100

    Update cheatsheet to v1.0.5

--- Casks/cheatsheet.rb
+++ Casks/cheatsheet.rb
@@ -1,9 +1,9 @@
 cask "cheatsheet" do
   arch arm: "aarch64", intel: "x64"
 
-  version "1.0.0"
-  sha256 arm:   "PLACEHOLDER_ARM64_SHA256",
-         intel: "PLACEHOLDER_X64_SHA256"
+  version "1.0.5"
+  sha256 arm:   "b3202d6e7701d0b338edb448d3579549758960362e0fb3fe0ab752dd4da72375",
+         intel: "a8a2ec23491ffaf99988cb05bebd230721b4b98c61b6aa910682059045f7f1b7  cheatsheet_1.0.5_x64.dmg"
 
   url "https://github.com/MagnusPladsen/my-cheatsheet/releases/download/v#{version}/cheatsheet_#{version}_#{arch}.dmg"
   name "Cheatsheet"
