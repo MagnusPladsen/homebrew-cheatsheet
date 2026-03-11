7a7e36fd7db27fc9cd46da338933fe55313cfea4
Magnus Pladsen <93226629+MagnusPladsen@users.noreply.github.com>
2026-03-11 11:05:47 +0100

    Update cheatsheet to v1.1.8

--- Casks/cheatsheet-app.rb
+++ Casks/cheatsheet-app.rb
@@ -1,14 +1,14 @@
 cask "cheatsheet-app" do
   arch arm: "aarch64", intel: "x64"
 
-  version "1.0.6"
-  sha256 arm:   "50172f8fd1a2d555d40e8c947db4a439c38c91b4535e748dab1b13b73027ac90",
-         intel: "9ea834a3ee31e0c84445341a34584f12cb0225a56466a522bb680e6b7ed64a5b"
+  version "1.1.8"
+  sha256 arm:   "b554aca9e893ce7fab9ba7176beaccf153f73a5df9871d5322beb19a34e9b336",
+         intel: "7d6ee2b5187e58cd506f2a6d3d65fe988b3fafae75615a3b78f7879f4ea7df26"
 
-  url "https://github.com/MagnusPladsen/my-cheatsheet/releases/download/v#{version}/cheatsheet_#{version}_#{arch}.dmg"
+  url "https://github.com/MagnusPladsen/cheatsheet-app/releases/download/v#{version}/cheatsheet_#{version}_#{arch}.dmg"
   name "Cheatsheet"
   desc "Developer keybinding cheatsheet for neovim, tmux, aerospace, and more"
-  homepage "https://github.com/MagnusPladsen/my-cheatsheet"
+  homepage "https://github.com/MagnusPladsen/cheatsheet-app"
 
   app "cheatsheet.app"
 
